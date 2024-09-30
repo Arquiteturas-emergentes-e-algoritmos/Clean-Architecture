49212442e54e0a40099306a58a33233155b71972
Kauã Vinícius <kauaponte@hotmail.com>
2024-09-30 18:23:24 -0300

    Fixes on repositories

---
+++ CleanArchitecture.Infrastructure/Repositories/GlucometerRepository.cs
@@ -0,0 +1,35 @@
+﻿using CleanArchitecture.Core.Glucometer;
+using CleanArchitecture.Infrastructure.Context;
+using CleanArchitecture.UseCases.Glucometer.Repositories;
+
+namespace CleanArchitecture.Infrastructure.Repositories;
+
+public class GlucometerRepository(DataContext context) : IGlucometerRepository
+{
+    private readonly DataContext _context = context;
+
+    public void Add(GlucoseTest entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(GlucoseTest entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<GlucoseTest> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public GlucoseTest GetById(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(GlucoseTest entity)
+    {
+        throw new NotImplementedException();
+    }
+}
