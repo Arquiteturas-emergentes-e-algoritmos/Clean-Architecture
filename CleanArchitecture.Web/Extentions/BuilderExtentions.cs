e9d4f47a2150346d436c2a704b2aaa76b2575c36
Kauã Vinícius <kauaponte@hotmail.com>
2024-09-30 18:14:35 -0300

    Web

---
+++ CleanArchitecture.Web/Extentions/BuilderExtentions.cs
@@ -0,0 +1,9 @@
+﻿namespace CleanArchitecture.Web.Extentions;
+
+public static class BuilderExtentions
+{
+    public static void AddDI(WebApplicationBuilder builder)
+    {
+        builder.Services.AddTransient<>();
+    }
+}
